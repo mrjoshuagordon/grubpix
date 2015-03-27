d140608dee2a4916f515f65fe58309a9dff1bb8f
Joshua Gordon <mr.joshuagordon@gmail.com>
2015-03-27 10:47:45 -0700

    Update mailtest.php

--- mailtest.php
+++ mailtest.php
@@ -1,5 +1,5 @@
 <?php
-mail('mr.joshuagordon@gmail.com','I can do anything I set my mind to - bitches aint shit!',
+mail('mr.joshuagordon@gmail.com','I can do anything!',
 'This is a test!', 'From: mr.joshuagordon@gmail.com');
 
 //echo phpinfo();
